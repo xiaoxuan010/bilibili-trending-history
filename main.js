7dfaba6e5f932ce5cec3335522f11dad5f55613d
xiaoxuan010 <2592053474@qq.com>
2023-01-03 22:33:56 +0800

    更新功能
    1. 添加brief简要文件输出
    2. 扩大记录范围为前30条热搜
    3. 内置定时器，每5秒更新一次
    4. 完善错误报告打印
    5. 优化日期时间打印
    6. 优化记录格式
    7. 删除部分弃用代码

--- main.js
+++ main.js
@@ -4,44 +4,54 @@ var moment = require('moment');
 
 //GET请求热搜数据
 function getTrending() {
-    BTrendingUrl = 'https://api.bilibili.com/x/web-interface/search/square?limit=10';
+    BTrendingUrl = 'https://api.bilibili.com/x/web-interface/search/square?limit=30';
     http.get(BTrendingUrl, (res) => {
         // console.debug('statusCode:', res.statusCode);
         // console.debug('headers:', res.headers);
-
-        //收到输入，调用记录函数
-        res.on('data', creatRecord);
+        var returnTime = res.headers.date;
+        //收到输入，先暂存后调用
+        var data = '';
+        res.on('data', (chunk) => { 
+            data += chunk;
+            // console.log('[获得返回数据]', d.toString()); 
+            
+        });
+        res.on('end', ()=>{
+            creatRecord(data, returnTime); 
+        });
     }).on('error', (e) => {
-        console.error(e);
+        console.error('[错误 001]', e);
     });
 }
 
 //创建记录
-function creatRecord(data) {
+function creatRecord(data, returnTime) {
     //生成记录数据
     //生成时间戳
     var nowTime = new moment();
     var nowTimeStamp = nowTime.format("X");
-    var nowTimeStr = nowTime.format("Y-MM-DDTHH:mm:ssZZ");
+    var nowTimeStr = nowTime.format("Y-MM-DD HH:mm:ssZZ");
     //生成热搜列表
     var dataStr = data.toString();
     var dataObj = JSON.parse(dataStr);
     var showNameArr = dataObj.data.trending.list.map(obj => { return obj.show_name });
 
     //标准输出
-    console.log("\n记录开始.")
-    console.log(nowTimeStamp, nowTimeStr);
-    showNameArr.forEach((elem, index) => {
+    console.log("\n记录开始.");
+    console.log("服务器时间: ", returnTime);
+    console.log("本地时间: ", nowTimeStr);
+    showNameArr.slice(0, 10).forEach((elem, index) => {
         console.log(index + 1, elem);
     });
     console.log("记录结束.\n")
 
     //文件输出
     //构造完整输出
-    var bilibiliTrendingHistory = '\n记录开始.\n'
-        + nowTimeStamp
-        + ' '
+    var bilibiliTrendingHistory = '\n记录开始.\n服务器时间: '
+        + returnTime + '\n本地时间: '
         + nowTimeStr
+        + ' '
+        + nowTimeStamp
         + '\n'
     showNameArr.forEach((showName, index) => {
         bilibiliTrendingHistory += `${index + 1}. ${showName}\n`;
@@ -54,19 +64,38 @@ function creatRecord(data) {
     try {
         fs.statSync('record/');
     } catch (error) {
-        //若目录不存在
-        // console.debug('record目录不存在，已创建.')
         fs.mkdirSync(`${__dirname}/record`);
     }
     //写入记录
     fs.writeFile(`record/${fileName}.log`, bilibiliTrendingHistory, { flag: 'a' }, err => {
         if (err) {
-            console.error(err);
-            return;
+            console.error('[错误 002]', err);
         }
-    })
+    });
+
+    //构造简要输出
+    var briefOutputStr = '时间: '
+        + nowTime.format('Y-MM-DD HH:mm:ss')
+        + '\n'
+    showNameArr.slice(0, 10).forEach((showName, index) => {
+        briefOutputStr += `${index + 1}. ${showName}\n`;
+    });
+    briefOutputStr += '\n';
+    var fileName = nowTime.format("Y-MM-DDTHH");
+    try {
+        fs.statSync('brief/');
+    } catch (error) {
+        fs.mkdirSync(`${__dirname}/brief`);
+    }
+    fs.writeFile(`brief/${fileName}.log`, briefOutputStr, { flag: 'a' }, err => {
+        if (err) {
+            console.error('[错误 003]', err);
+        }
+    });
+
     return;
 }
 
 //主入口
 getTrending();
+setInterval(getTrending, 5000);
