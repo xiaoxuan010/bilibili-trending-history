f2f2983e702b5149d4479f6d8ab943a27f4d9342
xiaoxuan010 <2592053474@qq.com>
2023-01-04 18:16:38 +0800

    优化记录格式

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var http = require("https");
 var fs = require("fs");
 var moment = require('moment');
 
+var lastTrending30 = new Array(30);
+var lastTrending10 = new Array(10);
+
 //GET请求热搜数据
 function getTrending() {
     BTrendingUrl = 'https://api.bilibili.com/x/web-interface/search/square?limit=30';
@@ -11,13 +14,13 @@ function getTrending() {
         var returnTime = res.headers.date;
         //收到输入，先暂存后调用
         var data = '';
-        res.on('data', (chunk) => { 
+        res.on('data', (chunk) => {
             data += chunk;
             // console.log('[获得返回数据]', d.toString()); 
-            
+
         });
-        res.on('end', ()=>{
-            creatRecord(data, returnTime); 
+        res.on('end', () => {
+            creatRecord(data, returnTime);
         });
     }).on('error', (e) => {
         console.error('[错误 001]', e);
@@ -35,15 +38,28 @@ function creatRecord(data, returnTime) {
     var dataStr = data.toString();
     var dataObj = JSON.parse(dataStr);
     var showNameArr = dataObj.data.trending.list.map(obj => { return obj.show_name });
+    var showNameArr10 = showNameArr.slice(0, 10);
+    var isSameAsLast30 = (showNameArr.toString() == lastTrending30.toString());
+    var isSameAsLast10 = (showNameArr10.toString() == lastTrending10.toString());
+    // console.log(isSameAsLast10, isSameAsLast30, lastTrending10, lastTrending30);
+    lastTrending30 = showNameArr;
+    lastTrending10 = showNameArr10;
 
     //标准输出
-    console.log("\n记录开始.");
-    console.log("服务器时间: ", returnTime);
-    console.log("本地时间: ", nowTimeStr);
-    showNameArr.slice(0, 10).forEach((elem, index) => {
-        console.log(index + 1, elem);
-    });
-    console.log("记录结束.\n")
+    if (isSameAsLast10) {
+        console.log("本地时间: ", nowTimeStr);
+        console.log('热搜前10未改变.')
+    }
+    else {
+        console.log("\n记录开始.");
+        console.log("服务器时间: ", returnTime);
+        console.log("本地时间: ", nowTimeStr);
+        showNameArr10.forEach((elem, index) => {
+            console.log(index + 1, elem);
+        });
+        console.log("记录结束.\n")
+    }
+
 
     //文件输出
     //构造完整输出
@@ -52,10 +68,15 @@ function creatRecord(data, returnTime) {
         + nowTimeStr
         + ' '
         + nowTimeStamp
-        + '\n'
-    showNameArr.forEach((showName, index) => {
-        bilibiliTrendingHistory += `${index + 1}. ${showName}\n`;
-    });
+        + '\n';
+    if (isSameAsLast30) {
+        bilibiliTrendingHistory += '热搜榜未发生改变.'
+    }
+    else {
+        showNameArr.forEach((showName, index) => {
+            bilibiliTrendingHistory += `${index + 1}. ${showName}\n`;
+        });
+    }
     bilibiliTrendingHistory += '\n原始数据：\n' + dataStr
         + '\n\n记录结束.\n';
     // console.debug(bilibiliTrendingHistory);
@@ -74,10 +95,12 @@ function creatRecord(data, returnTime) {
     });
 
     //构造简要输出
+    if (isSameAsLast30)
+        return;//若热搜一样则直接跳过
     var briefOutputStr = '时间: '
         + nowTime.format('Y-MM-DD HH:mm:ss')
         + '\n'
-    showNameArr.slice(0, 10).forEach((showName, index) => {
+    showNameArr.forEach((showName, index) => {
         briefOutputStr += `${index + 1}. ${showName}\n`;
     });
     briefOutputStr += '\n';
